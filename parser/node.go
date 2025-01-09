eb4cd4b1a94d1fe244a67d2eb386838526c08bd0
Adam Dickmeiss <adam@indexdata.dk>
2025-01-09 14:33:22 +0100

    XCQL and testing

--- parser/node.go
+++ parser/node.go
@@ -1,11 +1,14 @@
 package parser
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 const (
-	Top int = iota
-	Modifier
+	Modifier int = iota
 	BoolOp
 	SearchTerm
-	Sort
 )
 
 type Node struct {
@@ -14,4 +17,107 @@ type Node struct {
 	relation string
 	term     string
 	children []*Node
+}
+
+func cdata(sb *strings.Builder, msg string) {
+	pos := 0
+	for pos < len(msg) {
+		r, w := utf8.DecodeRuneInString(msg[pos:])
+		switch r {
+		case utf8.RuneError:
+			return
+		case '&':
+			sb.WriteString("&amp;")
+		case '<':
+			sb.WriteString("&lt;")
+		case '>':
+			sb.WriteString("&gt;")
+		default:
+			sb.WriteRune(r)
+		}
+		pos += w
+	}
+}
+
+func pr(sb *strings.Builder, level int, msg string) {
+	for i := 0; i < level; i++ {
+		sb.WriteString("  ")
+	}
+	sb.WriteString(msg)
+}
+
+func (n *Node) toXmlMod(sb *strings.Builder, level int) {
+	number := 0
+	for _, elem := range n.children {
+		if elem.kind == Modifier {
+			if number == 0 {
+				pr(sb, level, "<modifiers>\n")
+			}
+			pr(sb, level+1, "<modifier>\n")
+			number++
+			pr(sb, level+2, "<type>")
+			cdata(sb, elem.index)
+			pr(sb, 0, "<type>\n")
+			if len(elem.relation) > 0 {
+				pr(sb, level+2, "<comparison>")
+				cdata(sb, elem.relation)
+				pr(sb, 0, "</comparison>\n")
+			}
+			if len(elem.term) > 0 {
+				pr(sb, level+2, "<value>")
+				cdata(sb, elem.term)
+				pr(sb, 0, "</value>\n")
+			}
+			pr(sb, level+1, "</modifier>\n")
+		}
+	}
+	if number > 0 {
+		pr(sb, level, "</modifiers>\n")
+	}
+
+}
+
+func (n *Node) toXmlSb(sb *strings.Builder, level int) {
+	switch n.kind {
+	case SearchTerm:
+		pr(sb, level, "<searchClause>\n")
+		pr(sb, level+1, "<index>")
+		cdata(sb, n.index)
+		pr(sb, 0, "<index>\n")
+
+		pr(sb, level+1, "<relation>\n")
+		pr(sb, level+2, "<value>")
+		cdata(sb, n.relation)
+		pr(sb, 0, "</value>\n")
+		pr(sb, level+1, "</relation>\n")
+		n.toXmlMod(sb, level+1)
+		pr(sb, level+1, "<term>")
+		cdata(sb, n.term)
+		pr(sb, 0, "<term>\n")
+
+		pr(sb, level, "</searchClause>\n")
+	case BoolOp:
+		pr(sb, level, "<triple>\n")
+		pr(sb, level+1, "<boolean>\n")
+		pr(sb, level+2, "<value>")
+		cdata(sb, n.index)
+		pr(sb, 0, "</value>\n")
+		n.toXmlMod(sb, level+2)
+		pr(sb, level+1, "</boolean>\n")
+
+		pr(sb, level+1, "<leftOperand>\n")
+		n.children[0].toXmlSb(sb, level+2)
+		pr(sb, level+1, "</leftOperand>\n")
+
+		pr(sb, level+1, "<rightOperand>\n")
+		n.children[1].toXmlSb(sb, level+2)
+		pr(sb, level+1, "</rightOperand>\n")
+		pr(sb, level, "</triple>\n")
+	}
+}
+
+func (n *Node) ToXml() string {
+	var sb strings.Builder
+	n.toXmlSb(&sb, 0)
+	return sb.String()
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestQueries(t *testing.T) {
+func TestSimple(t *testing.T) {
 	var p Parser
 
 	node, err := p.Parse("beta")
@@ -15,6 +15,155 @@ func TestQueries(t *testing.T) {
 	_, err = p.Parse("")
 	if err == nil {
 		t.Errorf("expected error")
+	}
+}
+
+func TestParseXml(t *testing.T) {
+	var p Parser
+
+	for _, testcase := range []struct {
+		name   string
+		input  string
+		strict bool
+		ok     bool
+		expect string
+	}{
+		{
+			name:   "single term",
+			input:  "myterm",
+			strict: false,
+			ok:     true,
+			expect: `<searchClause>
+  <index>cql.serverChoice<index>
+  <relation>
+    <value>=</value>
+  </relation>
+  <term>myterm<term>
+</searchClause>
+`,
+		},
+		{
+			name:   "term rel value",
+			input:  "dc.title all andersen",
+			strict: false,
+			ok:     true,
+			expect: `<searchClause>
+  <index>dc.title<index>
+  <relation>
+    <value>all</value>
+  </relation>
+  <term>andersen<term>
+</searchClause>
+`,
+		},
+		{
+			name:   "relation modifiers",
+			input:  "dc.title =/k1=v1/k2 andersen",
+			strict: false,
+			ok:     true,
+			expect: `<searchClause>
+  <index>dc.title<index>
+  <relation>
+    <value>=</value>
+  </relation>
+  <modifiers>
+    <modifier>
+      <type>k1<type>
+      <comparison>=</comparison>
+      <value>v1</value>
+    </modifier>
+    <modifier>
+      <type>k2<type>
+    </modifier>
+  </modifiers>
+  <term>andersen<term>
+</searchClause>
+`,
+		},
+		{
+			name:   "booleans",
+			input:  "year < 1990 and b",
+			strict: false,
+			ok:     true,
+			expect: `<triple>
+  <boolean>
+    <value>and</value>
+  </boolean>
+  <leftOperand>
+    <searchClause>
+      <index>year<index>
+      <relation>
+        <value>&lt;</value>
+      </relation>
+      <term>1990<term>
+    </searchClause>
+  </leftOperand>
+  <rightOperand>
+    <searchClause>
+      <index>cql.serverChoice<index>
+      <relation>
+        <value>=</value>
+      </relation>
+      <term>b<term>
+    </searchClause>
+  </rightOperand>
+</triple>
+`,
+		},
+		{
+			name:   "prox",
+			input:  "(a prox/order=1 (b))",
+			strict: false,
+			ok:     true,
+			expect: `<triple>
+  <boolean>
+    <value>prox</value>
+    <modifiers>
+      <modifier>
+        <type>order<type>
+        <comparison>=</comparison>
+        <value>1</value>
+      </modifier>
+    </modifiers>
+  </boolean>
+  <leftOperand>
+    <searchClause>
+      <index>cql.serverChoice<index>
+      <relation>
+        <value>=</value>
+      </relation>
+      <term>a<term>
+    </searchClause>
+  </leftOperand>
+  <rightOperand>
+    <searchClause>
+      <index>cql.serverChoice<index>
+      <relation>
+        <value>=</value>
+      </relation>
+      <term>b<term>
+    </searchClause>
+  </rightOperand>
+</triple>
+`,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			node, err := p.Parse(testcase.input)
+			if testcase.ok {
+				if err != nil {
+					t.Fatalf("expected OK for query %s", testcase.input)
+				}
+				xml := node.ToXml()
+				if xml != testcase.expect {
+					t.Fatalf("Different XML for query %s\nExpect:\n%s\nGot:\n%s", testcase.input, testcase.expect, xml)
+				}
+			} else {
+				if err == nil {
+					t.Fatalf("expected Failure for query %s", testcase.input)
+				}
+			}
+		})
 	}
 
 }
